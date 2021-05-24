a287c99e6023de974103b3b3d4d3372bbcfd5a52
Michael West <mwjwest@gmail.com>
2021-05-23 20:58:36 -0600

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Directory - It's new $module
+
+# SYNOPSIS
+
+    use Directory;
+
+# DESCRIPTION
+
+Directory is ...
+
+# LICENSE
+
+Copyright (C) Michael West.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Michael West <mwjwest@gmail.com>
---
+++ lib/Directory.pm
@@ -0,0 +1,39 @@
+package Directory;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Directory - It's new $module
+
+=head1 SYNOPSIS
+
+    use Directory;
+
+=head1 DESCRIPTION
+
+Directory is ...
+
+=head1 LICENSE
+
+Copyright (C) Michael West.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Michael West E<lt>mwjwest@gmail.comE<gt>
+
+=cut
+
